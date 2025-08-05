badef2a26fa5f99a37dd0e37c67b208ed0706488
berraom <berrameroglu@icloud.com>
2025-08-05 07:00:36 +0300

    refactor: Rewrite entire file

--- src/app/components/socials.tsx
+++ src/app/components/socials.tsx
@@ -1,19 +1,27 @@
-import { FaInstagram, FaLinkedin, FaGithub, FaMedium, FaYoutube } from "react-icons/fa";
+import { FaInstagram, FaLinkedin, FaGithub, FaMedium, FaYoutube, FaTwitter } from "react-icons/fa";
 
 const SocialLinks = () => {
   return (
+    
     <div className="mt-10 flex gap-6 justify-center">
-      <a href="https://instagram.com/ayberkcakar" target="_blank" rel="noopener noreferrer">
+        <h3>FIND ME</h3>
+      <a href="https://www.instagram.com/ayberkcakar/" target="_blank" rel="noopener noreferrer">
         <FaInstagram size={30} className="hover:text-pink-500 transition" />
       </a>
-      <a href="https://linkedin.com/in/ayberkcakar" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.linkedin.com/in/ayberkcakar/" target="_blank" rel="noopener noreferrer">
         <FaLinkedin size={30} className="hover:text-blue-500 transition" />
       </a>
-      <a href="https://github.com/ayberkcakar" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/AyberkCakar" target="_blank" rel="noopener noreferrer">
         <FaGithub size={30} className="hover:text-gray-300 transition" />
       </a>
-      <a href="https://medium.com/@ayberkcakar" target="_blank" rel="noopener noreferrer">
+      <a href="https://x.com/ayberkcakar" target="_blank" rel="noopener noreferrer">
+        <FaTwitter size={30} className="hover:text-red-500 transition" />
+      </a>
+      <a href="https://ayberkcakar.medium.com/" target="_blank" rel="noopener noreferrer">
         <FaMedium size={30} className="hover:text-white transition" />
+      </a>
+      <a href="https://youtube.com/@ayberkcakar" target="_blank" rel="noopener noreferrer">
+        <FaYoutube size={30} className="hover:text-red-500 transition" />
       </a>
       <a href="https://youtube.com/@ayberkcakar" target="_blank" rel="noopener noreferrer">
         <FaYoutube size={30} className="hover:text-red-500 transition" />
