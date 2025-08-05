97d9c5d78be5fb2635fdcad8e6e75881e31f287e
berraom <berrameroglu@icloud.com>
2025-08-05 06:34:17 +0300

    feat:create about and head components.

---
+++ src/app/components/about.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+
+
+
+const   About = () => {
+    return (
+        <div>
+        
+        
+        <div className="paraghraph">
+            <p>Hi, I am a graduate of Manisa Celal Bayar University 
+            Software Engineering. Currently, I am studying for a
+            Master's Degree in Software Engineering at Manisa Celal
+            Bayar University. I work as a Full-Stack Developer at Supply 
+            Chain Wizard. I am interested in new technologies. I am trying 
+            to improve myself with the courses I follow from online course 
+            sites and with the software technology books I read. I am trying 
+            to develop projects and write medium articles with my new knowledge
+            that I have learned.</p> 
+            </div>
+        
+        <div className="image">
+            <Image className="picture" src="/ayberk.jpg" alt="Ayberk Çakar"/>
+        </div>
+        </div>
+    );
+};
+
+export default About;
+   
---
+++ src/app/components/socials.tsx
@@ -0,0 +1,18 @@
+const Socials = () => {
+  return (
+    <!-- Head içine Font Awesome CDN bağlantısı -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+
+<!-- Sosyal Medya İkonları -->
+<div class="social-icons">
+  <a href="#"><i class="fab fa-instagram"></i></a>
+  <a href="#"><i class="fab fa-linkedin-in"></i></a>
+  <a href="#"><i class="fab fa-github"></i></a>
+  <a href="#"><i class="fab fa-x-twitter"></i></a>
+  <a href="#"><i class="fab fa-youtube"></i></a>
+  <a href="#"><i class="fab fa-medium"></i></a>
+</div>
+    );
+};
+
+export default Socials;
