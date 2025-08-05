16fafc730633ed06cf71052cce12f0141fc8cbd5
berraom <berrameroglu@icloud.com>
2025-08-05 06:12:24 +0300

    feat: add Header component and remove empty About and Socials components

---
+++ src/app/components/header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+      <h1> WELCOME TO MY WORLD</h1>
+      <h2> Hi, I’m Ayberk Cakar
+a Software Engineer.Full-Stack Developer.</h2>
+    </div>
+    );
+  };
+  
+  export default Header;
